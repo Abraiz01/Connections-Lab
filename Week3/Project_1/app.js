37c37126f7b035e9eb02c60b9fc83881158eac01
Abraiz01 <78402495+Abraiz01@users.noreply.github.com>
2022-03-03 18:34:27 +0400

    Update app.js

--- Week3/Project_1/app.js
+++ Week3/Project_1/app.js
@@ -1,112 +1,324 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curiosity's Journey</title>
-    <link href="style.css" rel="stylesheet"/>
-</head>
+// declaring variable to store path of json file
+let jsonFile = "./results.json";
 
-<body>
-    <!-- div for the progress bar -->
-    <div class="p-bar">
-        <img class="earth" id="prog-earth" src="./photos/earth.png">
-        <progress id="prog-bar" value="0" max="100"></progress>
-        <img class="mars" id="prog-mars" src="./photos/mars.png">
-    </div>
+// declaring variable to store data inside json file
+let resultsData;
 
-    <!-- section containing elements of the first page (homepage)-->
-    <section class="page" id="page-1">
-        <blockquote class="blockquote__1" id="title-1">A Trip to Mars with Curiosity<span></span></blockquote>
-        <img class="earth" id="earth-1" src="./photos/earth.png">
+// methodology adopted from an example we did in class to detect if the json data is loaded
+let isDataReady = false;
 
-        <!-- teleportToMars function for the button is inside the app.js file -->
-        <button class="button button1" id = "button1" onclick="teleportToMars()">Teleport</button>
+// array to store objects from the 'Result' class
+let results = [];
 
-        <!-- About button that opens documentation -->
-        <button class="button button2" id = "button2" onclick="window.open('https://docs.google.com/document/d/1akf9JYemiadmf5Fm-Rhi2fc2gWSc6mYfSgWt2d3Pt0E/edit?usp=sharing');">About</button>
-        <blockquote id="blockquote-liftoff">Scroll Down to Lift Off<span></span></blockquote>
-    </section>
-
-    <!-- section containing elements of the second page (launch)-->
-    <section class="page" id="page-2">
-        <blockquote class="blockquote__1" id="blockquote-1">3, 2, 1, ... LIFTOFF! On Nov. 26, 2011 I left our planet in a spaceship called Atlas V<span></span></blockquote>
-        <blockquote class="blockquote__2" id="blockquote-2">Our journey is going to be a long one, so sit tight!<span></span></blockquote>
-        <img class="pic__1" id="launch-1" src="./photos/launch1.jpeg">
-        <img class="pic__2" id="launch-2" src="./photos/launch2.jpeg">
-        <img class="earth" id="earth-2" src="./photos/earth.png">
-        <img class="rocket" id="rocket-1" src="./photos/rocket.png">
-    </section>
-
-    <!-- section containing elements of the third page (rocket travelling)-->
-    <section class="page" id="page-3">
-        <blockquote class="blockquote__1" id="blockquote-3">Atlas V can carry me at a maximum speed of 47,000 miles per hour. At this rate, it would take me just 5 minutes to circle the globe!<span></span></blockquote>
-        <blockquote class="blockquote__2" id="blockquote-4">But even at this speed, it will take me about 7 months to reach Mars!<span></span></blockquote>
-        <img id="curiosity-1" src="./photos/curiosity.png">
-
-        <!-- Button that opens interactive 3d model of Curiosity in a new tab -->
-        <button class="button button4" id = "button4" onclick="window.open('model3d.html');" target="_blank">Click Here</button>
-        <blockquote class="blockquote__desc" id="blockquote-3d">To see me in 3d !<span></span></blockquote>
-        <img class="rocket" id="rocket-2" src="./photos/rocket-inv.png">
-    </section>
-
-    <!-- section containing elements of the fourth page (cruise spaceship)-->
-    <section class="page" id="page-4">
-        <blockquote class="blockquote__1" id="blockquote-5">I have now detached from the rocket in what's called a 'Cruise Ship'. This is the spaceship that will help me land on Mars.<span></span></blockquote>
-        <blockquote class="blockquote__2" id="blockquote-6">In the meantime, engineers back on Earth are making final adjustments to my landing path.<span></span></blockquote>
-        <img class="pic__1" id="cruise-pic1" src="./photos/cruise-2.jpeg">
-        <img class="pic__2" id="cruise-pic2" src="./photos/cruise-pic2.jpeg">
-        <img class="cruise" id="cruise-1" src="./photos/cruise.png">
-    </section>
-
-    <!-- section containing elements of the fifth page (landing)-->
-    <section class="page" id="page-5">
-        <blockquote class="blockquote__1" id="blockquote-7"> On August 5th 2012, I landed safely on Mars, 186 million miles away from home! You can see my landing site on the right.<span></span></blockquote>
-        <blockquote class="blockquote__2" id="blockquote-8">Now let's have a better look at what I discovered in Mars<span></span></blockquote>
-        <img class="pic__1" id="land-1" src="./photos/land-1.jpeg">
-        <img class="pic__2" id="land-2" src="./photos/pano.png">
-        <img class="mars" id="mars-1" src="./photos/mars.png">
-        <img class="cruise" id="cruise-2" src="./photos/cruise.png">
-    </section>
-
-    <!-- section containing elements of the final page -->
-    <section class="page" id="page-6">
-        <div id="canvas-container"></div>
-        <img class="mars__bg" id="mars-bg" src="./photos/results.png">
-        <img class="curiosity__rover" id="curiosity" src="./photos/curiosity.png">
-
-        <!-- teleportToEarth function for the button is inside the app.js file -->
-        <button class="button button3" id = "button3" onclick="teleportToEarth()">Back to Earth</button>
-
-        <!-- opaque button behind Curiosity to reload the page -->
-        <button class="button5" id = "button5" onclick="window.location.reload();">refresh</button>
-        <button class="button button6" id = "button6">What's This?</button>
-
-        <!-- creating a hidden div containing instructions and a '?' symbol, will be displayed when pointer is hovered over button6 -->
-        <div class="hidden">
-        <p id="question">?</p>
-        <p id="instructions">Hover over the Martian rocks to find out what I've discovered on Mars so far! Don't see the rocks? Click on me and I'll help you out.</p>
-        </div>
+window.addEventListener("load", () => {
+    fetch(jsonFile)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      resultsData = data;
+      isDataReady = true;
+  
+      // creating objects for the discoveries made by Curiosity
+      for(let i=0;i<resultsData.results.length;i++) {
+        results.push(new Result(resultsData.results[i].img_text, resultsData.results[i].img, resultsData.results[i].date, resultsData.results[i].text))
         
-    </section>
+      }
+  
+    })
+})
 
-    <!-- libraries and modules -->
 
-    <!-- Greensock and Scrollmagic libraries -->
-    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TimelineMax.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/animation.gsap.min.js"></script>
 
-    <!-- p5.js library -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"></script>
-    <script type="text/javascript" src="app.js"></script>
+/* --- code for GSAP and ScrollMagic starts here --- */
 
-    <!-- module for the interactive 3d model -->
-    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+// storing window height and width in their respective variables
+let height = window.innerHeight;
+let width = window.innerWidth;
 
+/* --- PAGE 2 --- */
+/* working of GSAP and Scrollmagic animation libraries */ 
+// initializing timeline variable for the current page
+// the timeline acts as a container for every object inside the page and allows us to
+// adjust their sequence and time delays using specific parameters
+// the argument contains the function updatePercentage, which keeps track of the scroll progress
+var tl2 = new TimelineMax({onUpdate:updatePercentage2});
+
+// creating a controller object that would control the scenes for each timeline
+// the scenes for each page define the scrolling activity of that page
+const controller = new ScrollMagic.Controller();
+
+// creating tween instances and defining parameters for the initial state of the animated object,
+// its speed, timing, opacity, and direction of motion
+tl2.from('#blockquote-1', .5, {x:200, opacity: 0});
+tl2.from('#blockquote-2', .5, {x:200, opacity: 0});
+tl2.from('#launch-1', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-1");
+tl2.from('#launch-2', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+tl2.from('#rocket-1', 1, {y:0.45*height, ease: Power4.easeInOut}, "=-.7");
+
+// creating a scene object for page-2 that allows the animations to play
+// once the user has scrolled enough to display the page entirely
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "#page-2",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin("#page-2")
+  .setTween(tl2)
+		.addTo(controller);
+
+// function to keep track of the scrolling progress made by the user
+function updatePercentage2() {
+  tl2.progress();
+  console.log(tl2.progress());
+}
+
+/* the same approach is followed for pages 3-5, 
+ to avoid redundancy, comments for page 2 are not repeated, but pages are labelled
+ when their code begins */
+
+/* --- PAGE 3 --- */
+
+var tl3 = new TimelineMax({onUpdate:updatePercentage3});
+
+tl3.from('#blockquote-3', .5, {x:200, opacity: 0});
+tl3.from('#blockquote-4', .5, {x:200, opacity: 0});
+tl3.from('#curiosity-1', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-1");
+tl3.from('#button4', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+tl3.from('#blockquote-3d', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+tl3.from('#rocket-2', 1, {y:-700, opacity: 0, ease: Power4.easeInOut}, "=-0.7");
+
+const scene3 = new ScrollMagic.Scene({
+  triggerElement: "#page-3",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin("#page-3")
+  .setTween(tl3)
+		.addTo(controller);
+
+
+function updatePercentage3() {
+  tl3.progress();
+  console.log(tl3.progress());
+}
+
+/* --- PAGE 4 --- */
+
+var tl4 = new TimelineMax({onUpdate:updatePercentage4});
+
+tl4.from('#blockquote-5', .5, {x:200, opacity: 0});
+tl4.from('#blockquote-6', .5, {x:200, opacity: 0});
+tl4.from('#cruise-pic1', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-1");
+tl4.from('#cruise-pic2', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+tl4.from('#cruise-1', 1, {y:-700, opacity: 0, ease: Power4.easeInOut}, "=-0.7");
+
+const scene4 = new ScrollMagic.Scene({
+  triggerElement: "#page-4",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin("#page-4")
+  .setTween(tl4)
+		.addTo(controller);
+
+
+function updatePercentage4() {
+  tl4.progress();
+  console.log(tl4.progress());
+}
+
+/* --- PAGE 5 --- */
+
+var tl5 = new TimelineMax({onUpdate:updatePercentage5});
+
+tl5.from('#blockquote-7', .5, {x:200, opacity: 0});
+tl5.from('#blockquote-8', .5, {x:200, opacity: 0});
+tl5.from('#land-1', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-1");
+tl5.from('#land-2', 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+tl5.from('#cruise-2', 1, {y:-500, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+
+const scene5 = new ScrollMagic.Scene({
+  triggerElement: "#page-5",
+            triggerHook: "onLeave",
+            duration: "100%"
+})
+  .setPin("#page-5")
+  .setTween(tl5)
+		.addTo(controller);
+
+
+function updatePercentage5() {
+  tl5.progress();
+  console.log(tl5.progress());
+}
+
+/* --- code for GSAP and ScrollMagic ends here --- */
+
+
+
+// initializing an earth variable for the earth button on the first page
+let earth;
+earth = document.getElementById('earth-1');
+
+// initializing 'teleport to mars' and 'game' buttons
+let button1;
+button1 = document.getElementById('button1');
+let button2;
+button2 = document.getElementById('button2');
+
+// adding mouseover event to make earth image opaque when the cursor
+// hovers over it
+earth.addEventListener("mouseover", function() {
+  earth.style.opacity = "0.5";
+});
+
+// returning earth opacity back to original state when cursor is no 
+// longer over earth
+earth.addEventListener("mouseout", function() {
+  earth.style.opacity = "1";
+});
+
+// making the 'teleport to mars' and 'game' buttons visible when earth is clicked
+earth.addEventListener("click", function() {
+  button1.style.display = "inline";
+  button2.style.display = "inline";
+});
+
+// function to scroll down to the last page where Curiosity is shown on Mars
+function teleportToMars() {
+  window.scrollTo({
+    top: 70000,
+    left: 100,
+    behavior: 'smooth'
+  });
+}
+
+// function to scroll up towards Earth to return to the first page
+function teleportToEarth() {
+  window.scrollTo({
+    top: 0,
+    left: 100,
+    behavior: 'smooth'
+  });
+}
+
+// initializing a curiosity variable for the curiosity rover button on the final page
+let curiosity;
+curiosity = document.getElementById('curiosity');
+
+// initializing 'teleport to earth' button
+let button3;
+button3 = document.getElementById('button3');
+
+// adding mouseover event to make Curiosity's image opaque when the cursor
+// hovers over it
+curiosity.addEventListener("mouseover", function() {
+  curiosity.style.opacity = "0.8";
+});
+
+// returning earth opacity back to original state when cursor is no 
+// longer over Curiosity
+curiosity.addEventListener("mouseout", function() {
+  curiosity.style.opacity = "1";
+});
+
+// making the 'teleport to earth' button visible when earth is clicked
+curiosity.addEventListener("click", function() {
+  button3.style.display = "inline";
+});
+
+
+// initializing a variable for the progress bar displayed in the top-right corner 
+let progressBar;
+progressBar = document.getElementById('prog-bar');
+
+// adding an event listener for a scroll event in order to make position calculations in the next steps
+window.addEventListener("scroll", () => {
+ 
+  // console.log(scrollY);
+  // console.log( (scrollY/window.innerHeight*6))
+
+  // formula was derived from trial and error by observing the values of scrollY in the 
+  // console log. There are 6 windows in total, so the value of the current scroll position is divided 
+  // by the total height of all windows, this rounded to about 50% (halfway through the progress bar)  
+  // at the start of the last page, so the final result was multiplied by 2
+  progressBar.value = String((scrollY/window.innerHeight*6)*2);
+})
+
+var sketchWidth;
+var sketchHeight;
+
+// setting up p5.js canvas
+function setup() {
+
+    sketchWidth = document.getElementById("canvas-container").offsetWidth;
+    sketchHeight = document.getElementById("canvas-container").offsetHeight;
+    var canvas = createCanvas(sketchWidth, sketchHeight);
+   
+    // Move the canvas so it’s inside our <section class="page" id="page-6">
+    canvas.parent('canvas-container');
+  
+}
+
+function draw() {
+    clear();
+    // background(0,0,0);
+  
+    if(isDataReady) {
+      for(let i=0;i<results.length;i++) {
+        results[i].drawImage();
+      }
+    }
+    for(let i=0;i<results.length;i++) {
+      results[i].showResult(mouseX, mouseY);
+    }
+}
+
+// update values of width and height dynamically as window is resized
+function windowResized() {
+  sketchWidth = document.getElementById("canvas-container").offsetWidth;
+  sketchHeight = document.getElementById("canvas-container").offsetHeight;
+  resizeCanvas(sketchWidth, sketchHeight);
+}
+
+// creating a class that will instantiate json file objects which contain Curiosity's result info
+class Result {
+    constructor(title,img,date,text) {
+      // this.img = loadImage(img);
+      this.img = loadImage(img);
+      this.rock = loadImage("./photos/rock.png");
+      this.rock_w = 0.04*sketchWidth
+      this.rock_h = 0.04*sketchWidth
+      this.title = title;
+      this.date = date;
+      this.text = text;
+      this.x = random(0.35*sketchWidth,0.9*sketchWidth);
+      this.y = random(0.55*sketchHeight,0.95*sketchHeight);
+      this.r = 0.02*sketchWidth;
+      
+    }
+
+    drawImage() {
+      image(this.rock, this.x - Math.floor(this.rock_w/2), this.y - Math.floor(this.rock_h/2), this.rock_w, this.rock_h);
+    }
     
-</body>
-</html>
+    // function to display contents of circles when they are clicked
+    showResult(mx, my) {
+      if(dist(mx, my, this.x, this.y) < this.r) {
+        
+        image(this.img, 0.04*sketchWidth, 0.03*sketchHeight, 0.25*sketchWidth, 0.35*sketchHeight);
+
+        textSize(0.022*sketchWidth);
+        fill(255,255,255)
+        text(this.title, 0.32*sketchWidth, 0.12*sketchHeight, 0.5*sketchWidth, 0.3*sketchHeight);
+
+        textSize(0.03*sketchWidth);
+        text(this.date, 0.32*sketchWidth, 0.08*sketchHeight);
+
+        textSize(0.015*sketchWidth);
+        text(this.text, 0.32*sketchWidth, 0.2*sketchHeight, 0.45*sketchWidth, 0.4*sketchWidth);
+      } 
+    }
+  
+  }
+
